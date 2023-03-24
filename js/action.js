ee8d6e9e80efc9888e8bf99c84923881bba029b0
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-24 21:34:05 +0600

    validation done

--- js/action.js
+++ js/action.js
@@ -6,7 +6,7 @@
 //if it is not input field then it is need to be access with .innerText
 //convert the value/innertext to number for calculation
 //calculate the value
-//set the value to display field
+//set the output value to display field
 //empty the input field
 
 document.getElementById('btn-deposit').addEventListener('click', function () {
@@ -39,21 +39,32 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const inputField2 = document.getElementById('withdraw-amount');
     const withdrawAmount = inputField2.value;
     const withdrawAmountInt = parseFloat(withdrawAmount);
-    console.log(withdrawAmountInt)
+    // console.log(withdrawAmountInt)
 
     const withdrawTotal1 = document.getElementById('withdrawTotal');
     const withdrawTotalValue = withdrawTotal1.innerText;
-    console.log(withdrawTotalValue);
+    // console.log(withdrawTotalValue);
     const winthdrawTotalflt = parseFloat(withdrawTotalValue);
+
+
+    const newbalance = document.getElementById('balance-total');
+    const newbalanceValue = newbalance.innerText;
+    const newbalanceValueFlt = parseFloat(newbalanceValue);
+
+
+    //validation
+    if (withdrawAmountInt > newbalanceValueFlt) {
+        alert('not enough money');
+        inputField2.value = '';
+        return;
+    }
     //add total of converted number
     const newTotal2 = withdrawAmountInt + winthdrawTotalflt;
     // console.log(newTotal2);
     withdrawTotal1.innerText = newTotal2;
     inputField2.value = '';
 
-    const newbalance = document.getElementById('balance-total');
-    const newbalanceValue = newbalance.innerText;
-    const newbalanceValueFlt = parseFloat(newbalanceValue);
+    //everything after condition should be written after condition
     const newbalance2 = newbalanceValueFlt - withdrawAmountInt;
     newbalance.innerText = newbalance2;
 })
